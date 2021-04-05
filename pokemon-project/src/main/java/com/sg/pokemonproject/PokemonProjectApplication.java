738c016606745ac9a664cdb2765bb6ea1158faf4
Veronica <veronicasmigielski@gmail.com>
2021-04-05 13:35:55 +0200

    Fixed Mapping & background Images, set up the display for admins partially (ability is done)

--- pokemon-project/src/main/java/com/sg/pokemonproject/PokemonProjectApplication.java
+++ pokemon-project/src/main/java/com/sg/pokemonproject/PokemonProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class PokemonProjectApplication {
 
 	public static void main(String[] args) {
