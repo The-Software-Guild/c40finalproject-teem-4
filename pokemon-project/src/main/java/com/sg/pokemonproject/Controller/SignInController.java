8d8f2fedbae10be694a584db7bf341dbf03cf5c2
John Turpin <johnturpin369@gmail.com>
2021-04-06 09:32:01 -0400

    Display alert when user enters incorrect email/password

--- pokemon-project/src/main/java/com/sg/pokemonproject/Controller/SignInController.java
+++ pokemon-project/src/main/java/com/sg/pokemonproject/Controller/SignInController.java
@@ -3,6 +3,7 @@ package com.sg.pokemonproject.Controller;
 import com.sg.pokemonproject.Dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,14 +16,19 @@ public class SignInController {
     UserDao userDao;
 
     @GetMapping("signin")
-    public String addUser(HttpServletRequest request){
+    public String addUser(HttpServletRequest request, Model model){
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+
+        if(email == null && password == null){
+            return "signin";
+        }
 
         if(userDao.login(email, password)){
             return "cards";
         }
         else{
+            model.addAttribute("badCredentials", true);
             return "signin";
         }
 
