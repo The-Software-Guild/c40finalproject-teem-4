72d04c600ff6ef793f462b300ebfe6535e209dc0
John Turpin <johnturpin369@gmail.com>
2021-04-08 12:00:03 -0400

    Now getting 2 abilities and type from API. Added condition so pokemon are loaded only once from api

---
+++ pokemon-project/src/main/java/com/sg/pokemonproject/models/Types.java
@@ -0,0 +1,13 @@
+package com.sg.pokemonproject.models;
+
+public class Types {
+    private JsonType type;
+
+    public JsonType getType() {
+        return type;
+    }
+
+    public void setType(JsonType type) {
+        this.type = type;
+    }
+}
